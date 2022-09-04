091bfbcb678b54e50df64950488ea1e17a283e67
Elena-Sea <smetyukelena@gmail.com>
2022-09-05 01:36:51 +0200

    adds minimal gallery styles

--- src/index.js
+++ src/index.js
@@ -11,116 +11,69 @@ const input = document.querySelector('input');
 const form = document.querySelector('form#search-form');
 const gallery = document.querySelector('.gallery');
 
+let imageType = '';
+let images = [];
 
 form.addEventListener('submit', searchImage);
-
-let imageType = '';
 input.addEventListener('input', debounce(onFormInput, DEBOUNCE_DELAY));
 
-function onFormInput(e) { 
-    imageType = e.target.value.trim();
-}
-
 axios.defaults.baseURL = 'https://pixabay.com/api';
-
 function fetchImages(imageType) { 
     console.log(imageType);
 
     return axios.get((`/?key=29710513-88fdd381238e9ed6d5c0ddb9e&q=${imageType}&image_type=photo&orientation=horizontal&safesearch=true`)).then(response => response.data)
 }
-let images = [];
+
+function onFormInput(e) { 
+    imageType = e.target.value.trim();
+}
+
 function searchImage(e) { 
     e.preventDefault();
-
-    // if (imageType === '') { 
-    //     resetMarkup();
-    //     return;
-    // };
+    if (imageType === '') { 
+        Notify.info("Please, fill in the field");
+        resetMarkup();
+        return;
+    };
     fetchImages(imageType).then(image => { return images = image.hits }).then(renderGallery).catch((error) => console.log(error));
 }
 
 function renderGallery() { 
+     if (images.length === 0) {
+        resetMarkup();
+        Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+    } else if (images.length >= 1) {
+        resetMarkup();
+        markupGallery(images);
+    } else {
+        Notify.info("Приятного просмотра");
+    }
+}
+
+function markupGallery() { 
     gallery.innerHTML = images.map(image => { 
         return `<div class="photo-card">
-                <img src="" alt="" loading="lazy" />
+                <div class="photo-card__image-container">
+                    <img class="photo-card__image" src="${image.largeImageURL}" alt="${image.tags}" loading="lazy" width="500"/>
+                </div>
                 <div class="info">
                     <p class="info-item">
-                    <b>Likes</b>
+                    <b class="info-item__name">Likes</b><span class="info-item__data">${image.likes}</span>
                     </p>
                     <p class="info-item">
-                    <b>Views</b>
+                    <b class="info-item__name">Views</b><span class="info-item__data">${image.views}</span>
                     </p>
                     <p class="info-item">
-                    <b>Comments</b>
+                    <b class="info-item__name">Comments</b><span class="info-item__data">${image.comments}</span>
                     </p>
                     <p class="info-item">
-                    <b>Downloads</b>
+                    <b class="info-item__name">Downloads</b><span class="info-item__data">${image.downloads}</span>
                     </p>
                 </div>
                 </div>`
     }).join('');
-
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const refs = {
-//     form: document.querySelector('form#search-form'),
-//     input: document.querySelector('input'),
-//     gallery: document.querySelector('.gallery'),
-// };
-
-// console.log(refs.gallery);
-// const DEBOUNCE_DELAY = 300;
-
-// refs.form.addEventListener('input', debounce(onFormInput, DEBOUNCE_DELAY));
-
-
-// // let inputName;
-
-// refs.form.addEventListener('submit', onFormSubmit);
-
-// function onFormInput(e) { 
-
-//     const inputName = e.target.value.trim();
-//     console.log(inputName);
-//     // if (inputName === '') { 
-//     //     resetMarkup();
-//     //     return;
-//     // }
-//     fetchImages(inputName).catch(error => {
-//         console.log(error.message);
-//     }); 
-// }
-
-// function onFormSubmit(e) { 
-//     e.preventDefault();
-//     if (!refs.input.value) { 
-//             return alert('Please fill in all fields!');
-//         }
-// }
-
-// function resetMarkup() { 
-//     gallery.innerHTML = '';
-// }
+function resetMarkup() { 
+    gallery.innerHTML = '';
+}
