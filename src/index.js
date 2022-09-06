cca937818cf83fe6b0972dacd6fb479f0d4015bd
Elena-Sea <smetyukelena@gmail.com>
2022-09-06 19:27:48 +0200

    adds improved code

--- src/index.js
+++ src/index.js
@@ -1,91 +1,58 @@
 import axios from 'axios';
 
-
-import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
 
-const DEBOUNCE_DELAY = 300;
-
-const input = document.querySelector('input');
 const form = document.querySelector('form#search-form');
 const gallery = document.querySelector('.gallery');
 const loadBtn = document.querySelector('button.load-more');
-const submitBtn = document.querySelector('button[type="submit"]');
 
 
-let imageType = '';
-let images = [];
-let page = 1;
-console.log(page);
-form.addEventListener('submit', showImages);
-input.addEventListener('input', debounce(onFormInput, DEBOUNCE_DELAY));
-loadBtn.addEventListener('click', markupGallery);
+form.addEventListener('submit', loadGallOnSubmit);
+loadBtn.addEventListener('click', uploadGallery);
 
+const reqOptions = {
+    key: '29710513-88fdd381238e9ed6d5c0ddb9e&q',
+    q: '',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: 40,
+    page: 1,
+}
 
 axios.defaults.baseURL = 'https://pixabay.com/api';
-function fetchImages(imageType) { 
-    console.log(imageType);
-    console.log(page);
-
-    return axios.get((`/?key=29710513-88fdd381238e9ed6d5c0ddb9e&q=${imageType}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`)).then(response => response.data);
-
-}
-console.log(page);
-
-function onFormInput(e) { 
-    imageType = e.target.value.trim();
-    if (imageType === '') { 
-        resetMarkup();
-        Notify.info("Please, type to search...");
-        submitBtn.disabled = false;
-        loadBtn.classList.add('visually-hidden');
-    };
-}
-
-function onSubmit(e) { 
-      e.preventDefault();
-    if (imageType === '') { 
-        Notify.info("Please, fill in the field");
-        resetMarkup();
-        return;
-    };
-    page += 1;
-}
-
-async function showImages(e) { 
-    onSubmit(e);
+async function fetchImages({ key, q, image_type, orientation, safesearch, per_page, page }) { 
     try {
-        const res = await fetchImages(imageType);
-        if (page > 1) { 
-            submitBtn.disabled = true;
-            loadBtn.classList.remove('visually-hidden');
-        }     
-        const numberFoundImages = res.totalHits;
-        const images = res.hits;
-        canRenderGallery(res, page);
-        markupGallery(images);
-    }
-    catch { 
-        error => console.log(error.message);
+        const res = await axios.get((`/?key=${key}&q=${q}&image_type=${image_type}&orientation=${orientation}&safesearch=${safesearch}&per_page=${per_page}&page=${page}`));
+        return res.data;
+    } catch (error) {
+        console.error(error);
     }
 }
 
-function canRenderGallery(res, page) { 
-    const maxNumberPages = Math.ceil(res.totalHits / res.hits.length);
-
-    console.log(maxNumberPages);
-
-    if (res.totalHits === 0) {
+async function loadGallOnSubmit(e) { 
+    e.preventDefault();
+    reqOptions.q = e.currentTarget.searchQuery.value.trim();
+    if (!e.currentTarget.searchQuery.value.length || !reqOptions.q) {
+            Notify.warning("Please, fill in the field");
+            reqOptions.page = 1;
+            resetMarkup();
+            return;
+        } else if (e.currentTarget.searchQuery.value.length >= 1) { 
+        reqOptions.page = 1;
         resetMarkup();
-        Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-        return;
-    } else if (res.totalHits > 1) {
-        Notify.success(`Hooray we found ${res.totalHits} images`);
-    } else if (page === maxNumberPages) {
-        Notify.info("We're sorry, but you've reached the end of search results.");
-    } else {
-        resetMarkup(images);
+     }
+     try { 
+        const res = await fetchImages(reqOptions);
+        if (reqOptions.page >= 1) { 
+            loadBtn.classList.remove('visually-hidden');
+        } 
+        notificationsInSubmit(res);
+        const galleryMarkup = await markupGallery(res.hits);
+        return galleryMarkup;
+    } catch { 
+        error => console.log(error.message);
     }
 }
 
@@ -114,6 +81,36 @@ function markupGallery(images) {
     );
 }
 
+async function uploadGallery(e) { 
+    reqOptions.page += 1;
+    try { 
+        const res = await fetchImages(reqOptions);        
+        const galleryUpload = await markupGallery(res.hits);
+        notificationsInUpload(res);
+    } catch { 
+        error => console.log(error.message);
+    }
+}
+
+
 function resetMarkup() { 
     gallery.innerHTML = '';
+}
+
+async function notificationsInSubmit(data) { 
+    if (data.totalHits === 0) {
+        resetMarkup();
+        Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+        return;
+    } else if (data.totalHits > 1) {
+        Notify.success(`Hooray we found ${data.totalHits} images`);
+    };
+}
+
+async function notificationsInUpload(data) {
+    const maxNumberPages = Math.ceil(data.totalHits / data.hits.length);
+    if (reqOptions.page === maxNumberPages) {
+        loadBtn.classList.add('visually-hidden');
+        Notify.info("We're sorry, but you've reached the end of search results.")
+    }
 }
